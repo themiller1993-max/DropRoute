bb3a903bd84a709bbd8035b03b37ab426eaf3c4c
themiller1993-max <themiller1993@gmail.com>
2026-08-20 07:26:10 +0100

    Add Mapbox beta app build

---
+++ mapbox-beta/app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.paul.droproute.mapboxbeta"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.paul.droproute.mapboxbeta"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 130
+        versionName = "1.3.0-beta"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("com.mapbox.maps:android:11.27.0")
+}
